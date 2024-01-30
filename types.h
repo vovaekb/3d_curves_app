c446e7699952b82583e401e9b896387dc8fc23bc
Vladimir Privalov <vovaprivalov@gmail.com>
2024-01-30 19:56:42 +0500

    Add getters for POint and Vector

--- types.h
+++ types.h
@@ -1,12 +1,19 @@
 #ifndef CADEX_TASK_TYPES_H
 #define CADEX_TASK_TYPES_H
 
+#include <iostream>
+
+using namespace std;
 
 class Point
 {
 public:
     Point() = default;
     Point(int x, int y, int z) : x_(x), y_(y), z_(z) {}
+    void print() { cout << x_ << ", " << y_ << ", " << z_ << endl; }
+    int getX() const { return x_; }
+    int getY() const { return y_; }
+    int getZ() const { return z_; }
 private:
     int x_;
     int y_;
@@ -18,6 +25,7 @@ class Vector
 public:
     Vector() = default;
     Vector(int x, int y, int z) : x_(x), y_(y), z_(z) {}
+    void print() { cout << x_ << ", " << y_ << ", " << z_ << endl; }
 private:
     int x_;
     int y_;
